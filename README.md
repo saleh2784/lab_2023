effd88b7b8c6942a17fc01993c61cb6fbac99098
root <root@Dell-6MYD0J3-SalehAbuShah>
2023-09-05 22:49:24 +0300

    aa

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # lab_2023
+k=kubectl
+k get pods
+k create ns saleh
+k get ns
+k apply -f deployment.yaml
+k delete -f deployment.yaml
+k get -n saleh pods
--- demo/app/app.py
+++ demo/app/app.py
@@ -6,10 +6,11 @@ app = Flask(__name__)
 host = os.getenv('HOSTNAME')
 var = os.getenv('MYVAR')
 secret = os.getenv('MYSECRET')
+env = os.getenv('env')
 
 @app.route('/')
 def index():
-    return render_template('index.html', host=host, var=var, secret=secret)
+    return render_template('index.html', host=host, var=var, secret=secret, env=env)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
